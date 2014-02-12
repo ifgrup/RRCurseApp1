0291ff3f9591b3c4ac22d0dbb19c3a1064092789
Roberto <ifgrup@gmail.com>
2014-02-12 18:04:40 +0100

    first commit
    first commit

---
+++ RRCurseApp1/RRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RRAppDelegate.h
+//  RRCurseApp1
+//
+//  Created by rrodriguez on 12/02/14.
+//  Copyright (c) 2014 EveronGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RRCurseApp1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RRCurseApp1
+//
+//  Created by rrodriguez on 12/02/14.
+//  Copyright (c) 2014 EveronGames. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RRAppDelegate class]));
+    }
+}
